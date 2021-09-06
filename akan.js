012e3d6a33bea17082a65ddb387f25529ddb09e0
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-09-06 15:08:58 +0300

    add a js link

---
+++ akan.js
@@ -0,0 +1,55 @@
+function myDate() {
+  var date = document.getElementById("date").value;
+  var gender= document.getElementById("gender").value;
+
+  if ( date == ""){
+    alert("please select valid date");
+    return;
+
+  }
+  console.log(date)
+  var myDay = new date(date);
+  var currentDay =myDay.getDay();
+
+  if (currentDay <("0 + currentDay">"31") {}
+    alert("Invalid Date");
+  }
+   displayName(currentDay, gender);
+
+  function displayName(currentDay, gender) {
+    if (gender =="male") {
+      if (currentDay == "0")
+      alert("Kwasi").onsubmit;
+      if (currentDay == "1")
+      alert("Kwadwo").onsubmit;
+      if (currentDay == "2")
+      alert("Kwabenna").onsubmit;
+      if (currentDay == "3")
+      alert("Kwaku").onsubmit;
+      if (currentDay == "4")
+      alert("Yaw").onsubmit;
+      if (currentDay == "5")
+      alert("Kofi").onsubmit;
+      if (currentDay == "6")
+      alert("Kwame").onsubmit;
+    }
+      else {
+        if(currentDay =="0")
+        alert("Akosua").onsubmit;
+        if (currentDay == "1")
+        alert("Adwoa").onsubmit;
+        if (currentDay == "2")
+        alert("Abenna").onsubmit;
+        if (currentDay == "3")
+        alert("Akua").onsubmit;
+        if (currentDay == "4")
+        alert("Yaa").onsubmit;
+        if (currentDay == "5")
+        alert("Afua").onsubmit;
+        if (currentDay == "6")
+        alert("Ama").onsubmit;
+      }
+
+
+    }
+  }
